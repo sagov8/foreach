5e9c7c2d929f8e007f43d3de080c77ff859ade54
sagov8 <sago.vivas@gmail.com>
2022-03-16 22:39:46 -0500

    initial commit

---
+++ foreach/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace @foreach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_mayor = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.txt_num3 = new System.Windows.Forms.TextBox();
+            this.txt_num2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_num1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCyan;
+            this.panel1.Controls.Add(this.txt_mayor);
+            this.panel1.Controls.Add(this.btn_calcular);
+            this.panel1.Controls.Add(this.txt_num3);
+            this.panel1.Controls.Add(this.txt_num2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txt_num1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(82, 92);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(574, 240);
+            this.panel1.TabIndex = 0;
+            // 
+            // txt_mayor
+            // 
+            this.txt_mayor.Location = new System.Drawing.Point(341, 121);
+            this.txt_mayor.Name = "txt_mayor";
+            this.txt_mayor.Size = new System.Drawing.Size(100, 20);
+            this.txt_mayor.TabIndex = 7;
+            
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(341, 74);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(100, 23);
+            this.btn_calcular.TabIndex = 6;
+            this.btn_calcular.Text = "Calcular Mayor";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // txt_num3
+            // 
+            this.txt_num3.Location = new System.Drawing.Point(165, 143);
+            this.txt_num3.Name = "txt_num3";
+            this.txt_num3.Size = new System.Drawing.Size(100, 20);
+            this.txt_num3.TabIndex = 5;
+            // 
+            // txt_num2
+            // 
+            this.txt_num2.Location = new System.Drawing.Point(165, 96);
+            this.txt_num2.Name = "txt_num2";
+            this.txt_num2.Size = new System.Drawing.Size(100, 20);
+            this.txt_num2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(105, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Número 3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Número 2";
+            // 
+            // txt_num1
+            // 
+            this.txt_num1.Location = new System.Drawing.Point(165, 51);
+            this.txt_num1.Name = "txt_num1";
+            this.txt_num1.Size = new System.Drawing.Size(100, 20);
+            this.txt_num1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Número 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(288, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dígite Números";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt_mayor;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.TextBox txt_num3;
+        private System.Windows.Forms.TextBox txt_num2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_num1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ foreach/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace @foreach
+{
+    public partial class Form1 : Form
+    {
+        int num1;
+        int num2;   
+        int num3;
+        List<int> numeros = new List<int>();
+        int mayor = 0;
+       public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToInt32(txt_num1.Text);
+            numeros.Add(num1);
+            num2 = Convert.ToInt32(txt_num2.Text);
+            numeros.Add(num2);
+            num3 = Convert.ToInt32(txt_num3.Text);
+            numeros.Add(num3);
+
+            mayor = numeros[1];
+
+            foreach (int i in numeros)
+            {
+                if (i > mayor)
+                {
+                    mayor = i; 
+                }
+            }   
+            
+            txt_mayor.Text = mayor.ToString();
+        }
+    }
+}
